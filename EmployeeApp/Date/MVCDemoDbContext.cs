aa292a739f6eedd9d1e8b8ceea0ef7e0e9ee40f9
Abdulmuheez Ogunrinde <abdulmuheezabiola@gmail.com>
2024-02-12 10:14:48 +0100

    Add project files.

---
+++ EmployeeApp/Date/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeApp.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApp.Date
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeApp/Migrations/20240124101806_changePhoneNumberTostring.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class changePhoneNumberTostring : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateofBirth",
+                table: "Employees",
+                newName: "DateOfBirth");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "Employees",
+                newName: "DateofBirth");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PhoneNumber",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
